7d169a792726ff28ee01598f0f8a394ba482e416
kblair-test <kath@glass-slipper.net>
2019-03-01 09:55:36 -0700

    First

---
+++ KBlairAssignment2/Assets/Scripts/Cardinal1.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cardinal1 : MonoBehaviour
+{
+    public float speed = 10;
+    public string word = "Hello World";
+    public string rly = "why World";
+    public bool currentlyFlying; // to see if it's flying
+    public bool currentlyMoving; // to see if it's flying
+    public GameObject firstSpot;
+    public GameObject secondSpot;
+    public GameObject thirdSpot;
+    public GameObject fourthSpot;
+    Animator animator;
+    Compass compass;
+    Gyroscope gyro;
+    public Vector3 gyroRot;
+    public float compassval;
+    public AudioSource myAudio;
+    public AudioSource myAudio2;
+    float lastsounded;
+    bool havesounded = false;
+    Renderer cardinalRenderer;
+    Material cardinalMaterial;
+    DeviceOrientation orientation;
+    DeviceOrientation targetOrientation = DeviceOrientation.FaceUp;
+    Color baseGrey;
+
+    public bool reachedDestination = false;
+
+   // public AudioClip successSound;
+
+    //private AudioSource source;
+    private Vector3 moveTarget;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+       // source = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        print("started Cardinal");
+        //set movetarget to the position of the first spot.
+        moveTarget = firstSpot.transform.position;
+        // rotate the bird to be looking at the right spot.
+        //transform.Rotate(0, 90, 0);
+        currentlyFlying = true;
+        currentlyMoving = true;
+        animator = GetComponent<Animator>();
+        gyro = Input.gyro;
+        compass = Input.compass;
+        compass.enabled = true;
+        gyro.enabled = true;
+        Input.location.Start();
+
+        cardinalRenderer = gameObject.GetComponentInChildren(typeof(Renderer)) as Renderer;
+        cardinalMaterial = cardinalRenderer.material;
+
+        ColorUtility.TryParseHtmlString("#B4B4B4", out baseGrey);
+
+        //animator.SetInteger("hop", 1);
+        //myAudio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // happens only when cardinal is active.
+        float step = speed * Time.deltaTime;
+
+        gyroRot = gyro.rotationRate;
+        compassval = compass.trueHeading;
+        orientation = Input.deviceOrientation;
+
+
+
+
+        if (currentlyFlying == true)
+        {
+            //going to need to be able to tell when I want to do this, and whether I should be rotating, etc.
+            transform.position = Vector3.MoveTowards(transform.position, moveTarget, step);
+        }
+        if(orientation == targetOrientation)
+        {
+            animator.SetTrigger("ruffle");
+        }
+        if (compassval < 180 && compassval > 90) //&& havesounded == false && reachedDestination == false
+        {
+            // need to time this
+            //myAudio2.Play();
+            //animator.SetTrigger("sing");
+            //havesounded = true;
+            //lastsounded = Time.time;
+            cardinalMaterial.color = Color.yellow;
+        }
+        else
+        {
+            cardinalMaterial.color = baseGrey;
+        }
+        /*
+        else if (Time.time - lastsounded > 5 && havesounded == true && reachedDestination == false)
+        {
+            // more than 5 seconds since last sounded, and we're out of the heading
+            //resent havesounded
+            havesounded = false;
+
+        }
+        */
+        /*
+        if (currentlyMoving == true && animator.GetInteger("hop") == 0)
+        {
+            animator.SetInteger("hop", 1);
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void CardinalEasyAR()
+    {
+        print("called from EasyAR");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Spot1"))
+        {
+            //set the target to spot2.
+            moveTarget = secondSpot.transform.position;
+            transform.LookAt(moveTarget);
+            //animator.SetTrigger("ruffle");
+
+        }
+        else if (other.gameObject.CompareTag("Spot2"))
+        {
+            moveTarget = thirdSpot.transform.position;
+            transform.LookAt(moveTarget);
+        }
+        else if (other.gameObject.CompareTag("Spot3"))
+        {
+            moveTarget = fourthSpot.transform.position;
+            transform.LookAt(moveTarget);
+        }
+        else if (other.gameObject.CompareTag("Spot4"))
+        {
+            print("in final position");
+            reachedDestination = true;
+            //GetComponent<AudioSource>().Play();
+            myAudio.Play(); 
+
+            currentlyMoving = false;
+            animator.SetTrigger("sing");
+        }
+    }
+
+}
---
+++ KBlairAssignment2/Assets/Scripts/NestScript.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NestScript : MonoBehaviour
+{
+    public Text accelText;
+    public Text micText;
+    public float accelxVal;
+    public float accelyVal;
+    public float accelzVal;
+    public float micLev;
+    public float db;
+    public float compassval;
+    Compass compass;
+    Gyroscope gyro;
+    public Vector3 gyroRot;
+    Material cardinalMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("started nestscript");
+        accelxVal = 0;
+        accelyVal = 0;
+        accelzVal = 0;
+        SetAccelText();
+        SetMicText();
+
+        //Set up and enable the gyroscope (check your device has one)
+        gyro = Input.gyro;
+        compass = Input.compass;
+        compass.enabled = true;
+        gyro.enabled = true;
+        Input.location.Start();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //accelxVal = Input.acceleration.x;
+        //accelyVal = Input.acceleration.y;
+        //accelzVal = Input.acceleration.z;
+        gyroRot = gyro.rotationRate;
+        
+        compassval = compass.trueHeading;
+        SetAccelText();
+        //micText.text = Microphone.devices.ToString();
+        db = 20 * Mathf.Log10(Mathf.Abs(MicInput.MicLoudness));
+        //micText.text = "g: " + gyroRot.ToString();
+        micText.text = "c: " + compassval.ToString();
+        //SetMicText();
+        print(Microphone.devices);
+        print(db);
+        //accelText.text = "Accelerometer: x[" + Input.acceleration.x.ToString();
+    }
+
+    void SetAccelText()
+    {
+        //accelText.text = "a: " + gyroRot.ToString();
+        accelText.text = Input.deviceOrientation.ToString();
+    }
+
+    void SetMicText()
+    {
+        //for now not mic
+        //float db = 20 * Mathf.Log10(Mathf.Abs(MicInput.MicLoudness));
+        micText.text = "Mic level: " + db;
+        //micText.text
+    }
+}
+
+//This code is thanks to user atomtwist at https://forum.unity.com/threads/check-current-microphone-input-volume.133501/
+public class MicInput : MonoBehaviour
+{
+
+    public static float MicLoudness;
+
+    private string _device;
+
+    //mic initialization
+    void InitMic()
+    {
+        if (_device == null) _device = Microphone.devices[0];
+        _clipRecord = Microphone.Start(_device, true, 999, 44100);
+    }
+
+    void StopMicrophone()
+    {
+        Microphone.End(_device);
+    }
+
+    //may need to get rid of this
+    AudioClip _clipRecord;
+    int _sampleWindow = 128;
+
+    //get data from microphone into audioclip
+    float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1); // null means the first microphone
+        if (micPosition < 0) return 0;
+        _clipRecord.GetData(waveData, micPosition);
+        // Getting a peak on the last 128 samples
+        for (int i = 0; i < _sampleWindow; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+
+
+    void Update()
+    {
+        // levelMax equals to the highest normalized value power 2, a small number because < 1
+        // pass the value to a static var so we can access it from anywhere
+        MicLoudness = LevelMax();
+    }
+
+    bool _isInitialized;
+    // start mic when scene starts
+    void OnEnable()
+    {
+        InitMic();
+        _isInitialized = true;
+    }
+
+    //stop mic when loading a new level or quit application
+    void OnDisable()
+    {
+        StopMicrophone();
+    }
+
+    void OnDestroy()
+    {
+        StopMicrophone();
+    }
+
+
+    // make sure the mic gets started & stopped when application gets focused
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            //Debug.Log("Focus");
+
+            if (!_isInitialized)
+            {
+                //Debug.Log("Init Mic");
+                InitMic();
+                _isInitialized = true;
+            }
+        }
+        if (!focus)
+        {
+            //Debug.Log("Pause");
+            StopMicrophone();
+            //Debug.Log("Stop Mic");
+            _isInitialized = false;
+
+        }
+    }
+}
